892791bf9c98b8decb284338811118bf1756fe7d
yashtyagi298 <tyagiyash097@gmail.com>
2025-10-02 16:48:00 +0530

    setup done

---
+++ src/test/java/com/example/e_com/EComApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e_com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
