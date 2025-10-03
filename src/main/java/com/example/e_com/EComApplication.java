00ec842fa6096f9e29e0d069ba38ba4b87fcead9
yashtyagi298 <tyagiyash097@gmail.com>
2025-10-03 16:54:11 +0530

    added dotenv setup

--- src/main/java/com/example/e_com/EComApplication.java
+++ src/main/java/com/example/e_com/EComApplication.java
@@ -1,5 +1,7 @@
 package com.example.e_com;
 
+
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EComApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load(); // load env var from .env file
+		dotenv.entries().forEach(entry-> System.setProperty(entry.getKey(),entry.getValue())); // set system properties from env var and set them
 		SpringApplication.run(EComApplication.class, args);
 	}
 
